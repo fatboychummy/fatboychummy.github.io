113225e0b0d7d4530bbccc40928a49558ab47ce0
kenzo44 <kenzo44@users.noreply.github.com>
2021-03-14 01:55:33 -0700

    Fixed box shadow

--- etc/Moodle-Cat-Thing/Cat.js
+++ etc/Moodle-Cat-Thing/Cat.js
@@ -12,7 +12,7 @@ for (i = 0; i < possibleParents.length; i++) {
 const addCSS = s => document.head.appendChild(document.createElement("style")).innerHTML=s;
 
 // Usage: 
-addCSS("#FAT_IMAGE{display:block !important; margin-left:auto !important; margin-right:auto !important; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19) !important;}")
+addCSS("#FAT_IMAGE{display:block !important; margin-left:auto !important; margin-right:auto !important; box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12) !important;}")
   
 // get the FAT_IMAGE
 var img = document.getElementById("FAT_IMAGE");
